f65155dd7b1f5177f694f381735179606ad5c259
KK1234-stack <133097498+KK1234-stack@users.noreply.github.com>
2025-01-03 12:12:25 +0530

    Update Ethernet.py
    converted protocols to human readable form

--- Ethernet.py
+++ Ethernet.py
@@ -2,27 +2,32 @@ import socket
 import struct
 import textwrap
 
-
-
 def main():
-    conn=socket.socket[socket.AF_PACKET,socket.SOCK_RAW,socket.htons(3)]
+    conn = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.htons(3))
     while True:
-        raw_data,addr=conn.recvfrom(65536)
-        dest_mac,src_mac,eth_proto,data=ethernet_frame(raw_data)
-        print('\n Ethernet frame: ')
-        print('Destination: {}, Source: {}, Protocol: {}'.format(dest_mac,src_mac,eth_proto))
+        raw_data, addr = conn.recvfrom(65536)
+        dest_mac, src_mac, eth_proto, data = ethernet_frame(raw_data)
+        print('\nEthernet frame:')
+        print('Destination: {}, Source: {}, Protocol: {}'.format(dest_mac, src_mac, eth_proto))
 
-#unpack ethernet frame 
-
+# Unpack Ethernet frame
 def ethernet_frame(data):
-    dest_mac,src_mac,prototyp= struct.unpack('! 6s 6s H',data[:14])
-    return get_mac_addr(dest_mac),get_mac_addr(src_mac),socket.htons(prototyp),data[14:] # aactual payload is data[14:]
+    dest_mac, src_mac, eth_proto = struct.unpack('!6s6sH', data[:14])  # Unpack Ethernet header
+    return get_mac_addr(dest_mac), get_mac_addr(src_mac), eth_protocol(eth_proto), data[14:]  # Payload is data[14:]
 
-# to return mac address in proper formatting AA:B1:43:DE:3A:B9
-
+# Convert MAC address to string format
 def get_mac_addr(bytes_addr):
-    bytes_str= map('{:02x}'.format, bytes_addr)
+    bytes_str = map('{:02x}'.format, bytes_addr)
     return ':'.join(bytes_str).upper()
 
-main()
-     
+# Convert protocol number to human-readable format
+def eth_protocol(proto):
+    eth_protocols = {
+        0x0800: 'IPv4',
+        0x0806: 'ARP',
+        0x86DD: 'IPv6',
+    }
+    return eth_protocols.get(proto, 'Unknown')
+
+if __name__ == '__main__':
+    main()
